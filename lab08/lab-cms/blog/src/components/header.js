53e0e3a9ccaed8f2c451c99356cff691124c68d3
arranay <valeriakhlyzoba@gmail.com>
2020-05-30 12:01:40 +0300

    adding entities

--- lab08/lab-cms/blog/src/components/header.js
+++ lab08/lab-cms/blog/src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          Основы разработки веб приложений
         </Link>
       </h1>
     </div>
--- lab08/lab-cms/blog/src/pages/index.js
+++ lab08/lab-cms/blog/src/pages/index.js
@@ -1,22 +1,24 @@
 import React from 'react'  
-import Link from 'gatsby-link'
-const IndexPage = ({ data }) => (  
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+
+const IndexPage = ({ data }) => ( 
+  <Layout> 
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Лабораторная работа №8.</p>
+    <p>Основы разработки с Headless CMS.</p>
     <ul>
       {data.allStrapiPosts.edges.map(document => (
         <li key={document.node.id}>
           <h2><font color="#3AC1EF">
             <Link to={`/${document.node.id}`}>{document.node.tittle}</Link>
           </font></h2>
-          <p>{document.node.content}</p>
         </li>
       ))}
     </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
+  </Layout>
 )
 export default IndexPage
 export const pageQuery = graphql`  
